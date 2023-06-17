e3c85e51c24ed2fb14347c677fa90102e93443a7
ruzi <rozbeh24@outlook.com>
2023-06-17 22:28:19 +0330

    soekt io

--- src/controllers/socket.io/publisher.socket.ts
+++ src/controllers/socket.io/publisher.socket.ts
@@ -2,7 +2,9 @@
 import { Socket } from "socket.io";
 export function getPub(socket: Socket, msg: any, ack: any) {
   console.log('hi', msg);
-
-  ack(msg+'hi')
+  if (typeof ack=='function') {
+    ack(msg+'hi')
+  }
+  
   //return  socket.to(msg.roomId).emit("newMsg", msg.caption);
 }

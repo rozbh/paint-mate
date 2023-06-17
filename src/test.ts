6faa234321d1bb75cb4464830c6fa6a36aba8cd0
farshadBish <farshadota@gmail.com>
2023-06-17 16:04:30 +0200

    new interface

---
+++ src/test.ts
@@ -0,0 +1,16 @@
+interface PaintInterface {
+    x: number,
+    y: number,
+    color: string,
+    lineWidth: number
+} 
+
+
+const x: PaintInterface[] = [
+    {
+        x: 12,
+        y: 13,
+        color: '#0000',
+        lineWidth:0
+    }
+]
